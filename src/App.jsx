7e98104a247e47c562fcd56980fceb4e6e761203
Ravi Kumar <ravi14577@gmail.com>
2024-03-08 11:58:15 +0000

    basic structure init

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="p-8 max-w-xs mx-auto">
+        <h1 className="text-3xl font-bold mb-4">TodoMatic</h1>
+        <p className="text-gray-700">What needs to be done?</p>
+
+        <div className="flex mt-4">
+          <input type="text" className="border-2 border-gray-300 p-2 flex-1"  />
+          <button className="ml-2 bg-blue-500 text-white p-2 rounded">Add</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
